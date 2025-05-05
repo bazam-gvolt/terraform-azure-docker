0aa36d5cba02a9e13c18e84fd1644b290bf11dbe
reddomeuk <academy@reddome.org>
2025-05-05 17:30:48 +0100

    rls11

--- modules/monitoring/cloudflare.tf
+++ modules/monitoring/cloudflare.tf
@@ -8,9 +8,9 @@ resource "helm_release" "cloudflared_grafana" {
 
   values = [
     templatefile("${path.module}/values/cloudflare-grafana-values.yaml", {
-      CLOUDFLARE_TUNNEL_TOKEN = var.cloudflare_tunnel_token_grafana
-      GRAFANA_SUBDOMAIN      = var.grafana_subdomain
-      DOMAIN_NAME           = var.domain_name
+      CLOUDFLARE_TUNNEL_TOKEN_GRAFANA = var.cloudflare_tunnel_token_grafana
+      GRAFANA_SUBDOMAIN               = var.grafana_subdomain
+      DOMAIN_NAME                     = var.domain_name
     })
   ]
 
@@ -27,9 +27,9 @@ resource "helm_release" "cloudflared_wazuh" {
 
   values = [
     templatefile("${path.module}/values/cloudflare-wazuh-values.yaml", {
-      CLOUDFLARE_TUNNEL_TOKEN = var.cloudflare_tunnel_token_wazuh
-      WAZUH_SUBDOMAIN        = var.wazuh_subdomain
-      DOMAIN_NAME           = var.domain_name
+      CLOUDFLARE_TUNNEL_TOKEN_WAZUH = var.cloudflare_tunnel_token_wazuh
+      WAZUH_SUBDOMAIN               = var.wazuh_subdomain
+      DOMAIN_NAME                   = var.domain_name
     })
   ]
 
