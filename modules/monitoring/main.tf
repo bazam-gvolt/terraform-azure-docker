b81091ec28c74b44ec7ff1779879561727ba1fb0
reddomeuk <academy@reddome.org>
2025-05-05 22:00:43 +0100

    rls29

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -8,50 +8,172 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-resource "helm_release" "grafana" {
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-  version    = "6.50.7"
-  timeout    = 900  # Increase timeout to 15 minutes
-  wait       = false  # Don't wait for all resources to be ready
+# Instead of using Helm for Grafana, we'll use a Kubernetes deployment directly
+# This gives us more control over resource limits for all containers
+resource "kubernetes_deployment" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      app = "grafana"
+    }
+  }
 
-  values = [
-    templatefile("${path.module}/values/grafana-values.yaml", {
-      GRAFANA_ADMIN_PASSWORD = var.grafana_admin_password
-    })
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "grafana"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "grafana"
+        }
+      }
+
+      spec {
+        init_container {
+          name  = "init-chown-data"
+          image = "busybox:1.35"
+          
+          # Add explicit resource limits for init container
+          resources {
+            limits = {
+              cpu    = "200m"
+              memory = "256Mi"
+            }
+            requests = {
+              cpu    = "100m"
+              memory = "128Mi"
+            }
+          }
+          
+          command = ["sh", "-c", "chown -R 472:472 /var/lib/grafana"]
+          
+          volume_mount {
+            name       = "grafana-storage"
+            mount_path = "/var/lib/grafana"
+          }
+        }
+
+        container {
+          name  = "grafana"
+          image = "grafana/grafana:9.3.6"
+          
+          # Add explicit resource limits for main container
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "1Gi"
+            }
+            requests = {
+              cpu    = "200m"
+              memory = "500Mi"
+            }
+          }
+          
+          port {
+            container_port = 3000
+            name           = "http-grafana"
+            protocol       = "TCP"
+          }
+
+          env {
+            name  = "GF_SECURITY_ADMIN_PASSWORD"
+            value = var.grafana_admin_password
+          }
+          
+          env {
+            name  = "GF_INSTALL_PLUGINS"
+            value = "grafana-clock-panel,grafana-simple-json-datasource"
+          }
+
+          volume_mount {
+            name       = "grafana-storage"
+            mount_path = "/var/lib/grafana"
+          }
+        }
+
+        volume {
+          name = "grafana-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.grafana_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_namespace.monitoring,
+    kubernetes_persistent_volume_claim.grafana_pvc
   ]
+}
 
-  set {
-    name  = "persistence.enabled"
-    value = "true"
+# Create persistent volume claim for Grafana
+resource "kubernetes_persistent_volume_claim" "grafana_pvc" {
+  metadata {
+    name      = "grafana-storage"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+    storage_class_name = "managed-premium"
+  }
+  depends_on = [kubernetes_namespace.monitoring]
+}
+
+# Create Grafana service
+resource "kubernetes_service" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = "grafana"
+    }
+    port {
+      port        = 3000
+      target_port = 3000
+      protocol    = "TCP"
+    }
+    type = "ClusterIP"
+  }
+  depends_on = [kubernetes_deployment.grafana]
+}
+
+# Configure Grafana datasources through ConfigMap
+resource "kubernetes_config_map" "grafana_datasources" {
+  metadata {
+    name      = "grafana-datasources"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      app = "grafana"
+    }
   }
 
-  set {
-    name  = "persistence.size"
-    value = "10Gi"
-  }
-  
-  # Add resource configurations directly
-  set {
-    name  = "resources.requests.cpu"
-    value = "200m"
-  }
-  
-  set {
-    name  = "resources.requests.memory"
-    value = "500Mi"
-  }
-  
-  set {
-    name  = "resources.limits.cpu"
-    value = "500m"
-  }
-  
-  set {
-    name  = "resources.limits.memory"
-    value = "1Gi"
+  data = {
+    "datasources.yaml" = <<-EOF
+      apiVersion: 1
+      datasources:
+      - name: Wazuh
+        type: elasticsearch
+        url: http://wazuh-indexer-indexer:9200
+        database: wazuh-*
+        jsonData:
+          timeField: "@timestamp"
+          esVersion: "7.10.2"
+    EOF
   }
 
   depends_on = [kubernetes_namespace.monitoring]
@@ -89,4 +211,28 @@ resource "kubernetes_config_map" "lab_documentation" {
   }
 
   depends_on = [kubernetes_namespace.monitoring]
+}
+
+# For Wazuh deployment, we need to create the resources directly
+resource "kubernetes_namespace" "wazuh" {
+  metadata {
+    name = "wazuh"
+  }
+}
+
+# We'll create the Wazuh resources directly using kubectl apply
+resource "null_resource" "deploy_wazuh" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      mkdir -p ${path.module}/wazuh-temp
+      cd ${path.module}/wazuh-temp
+      git clone https://github.com/wazuh/wazuh-kubernetes.git -b v4.5.1 --depth=1
+      cd wazuh-kubernetes
+      kubectl apply -f wazuh/base/
+      kubectl apply -f wazuh/indexer_stack/
+      kubectl apply -f wazuh/manager/
+    EOT
+  }
+
+  depends_on = [kubernetes_namespace.wazuh]
 }
